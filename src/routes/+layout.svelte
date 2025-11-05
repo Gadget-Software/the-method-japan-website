bd44fc56de5e275f43f83e0c8dfd9615a9c7216c
Sanath Sambamoorthi <sanath001@gmail.com>
2025-11-05 20:54:52 +0900

    Import CSS and update meta tags in layout
    Added CSS import for styling and updated meta tags.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
   import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
+  import '../app.css';
 </script>
+
+<slot />
 
 <svelte:head>
   <title>The Method</title>
